6c2777753a9c3522d21b23294882153741a4cf2a
sunithprabhu <sunithprabhu@gmail.com>
2020-08-11 17:17:33 +0530

    from visual studio code

---
+++ ForTestinf.java
@@ -0,0 +1,18 @@
+package com.prabhu.co;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ForTestinf {
+
+@Test
+
+public void doSomething()
+{
+	int a = 10;
+	assertEquals("good",a,10);
+}
+	
+}
+
